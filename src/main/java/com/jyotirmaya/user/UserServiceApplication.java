604141b15ffb2d3cd084e7ec0fe4ceed127e65b9
jyotirmaya <jyotirmayap001@gmail.com>
2024-02-24 22:46:35 +0530

    added eureka client details

--- src/main/java/com/jyotirmaya/user/UserServiceApplication.java
+++ src/main/java/com/jyotirmaya/user/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.jyotirmaya.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
